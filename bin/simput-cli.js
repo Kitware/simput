765b5e5425a4856c7e786e4c245a27e41c5551fa
TristanWright <tristan.wright@kitware.com>
2016-01-25 16:17:09 -0700

    fix(cli, pyfr): new pyfr with default externals, cli fix where there's no -i

--- bin/simput-cli.js
+++ bin/simput-cli.js
@@ -7,7 +7,7 @@ const path = require('path'),
       program = require('commander'),
       express = require('express'),
       bodyParser = require('body-parser');
-      
+
 const simputCompiler  = require('./compile'),
       simputManager   = require('./manager'),
       simputConverter = require('./converter');
@@ -32,11 +32,11 @@ program
   .option('-i, --input [file|directory]',  'Input file or directory')
   .option('-o, --output [directory]', 'Output directory to output to')
   .option('-t, --type [type]',   'Type of input\n')
-  
+
   .option('-n, --no-gui', 'Just generate output')
   .option('-s, --silent', 'Do not open the browser')
   .option('-p, --port [8080]', 'Server port\n', (n) => Number(n), 8080)
-  
+
   //management
   .option('-c, --compile [directory]','Directory to compile files')
   .option('-a, --add [file]',    'Add model to list of available inputs')
@@ -45,7 +45,7 @@ program
   //.option('-g --generate', 'Generate blank language files from the model')
 
   .parse(process.argv);
-  
+
 if (!process.argv.slice(2).length) {
     program.outputHelp();
     process.exit(0);
@@ -60,11 +60,11 @@ if (program.input && program.output && !program.gui) {
   const app = express();
   app.use(bodyParser.json({limit: 10000000}));
   app.use(express.static(__dirname + "/../dist"));
-    
+
   ls(simputFolder).forEach(function(file) {
       require(path.join(simputFolder, file));
   });
-  
+
   var inputFile = null;
   if (program.input) {
     //load input
@@ -74,25 +74,34 @@ if (program.input && program.output && !program.gui) {
       inputFile = require(path.join(process.env.PWD, program.input));
     }
   }
-  
+
   if (inputFile === null) {
     console.log('No valid json found in ' + program.output);
   }
 
   app.route('/type/:type')
     .get(function(req, res) {
-      var fileToServe = path.join(simputFolder, req.params.type);
-      res.sendFile(fileToServe);
+      var homeFile = path.join(simputFolder, req.params.type),
+        distFile = path.join(__dirname, '../dist', 'types', req.params.type);
+
+      if (test('-f', homeFile)) {
+        res.sendFile(homeFile);
+      } else if (test('-f', distFile)) {
+        res.sendFile(distFile);
+      } else {
+        res.status(404).send('no files of given type "' + req.params.type + '" found');
+      }
+
     });
-  
+
   app.route('/data')
     .get(function(req, res) {
-      if (program.input) { 
+      if (program.input) {
         // send input file
         res.send({input: true, data: inputFile});
       } else {
         // send options for input
-        res.send({input: false, data: Object.keys(GLOBAL.Simput.types)});
+        res.send({input: true, data: {type: program.type, data:{}} });
       }
     })
     .post(function(req, res) {
@@ -103,10 +112,10 @@ if (program.input && program.output && !program.gui) {
       }
       res.send('Data saved');
     });
-    
+
   app.listen(program.port);
   console.log('Simput listening on port ' + program.port);
-  
+
   if (!program.silent) {
       require('open')('http://localhost:' + program.port);
   }
