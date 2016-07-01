2e8852a5648d036d52b918407acde6ebee2f5bf5
TristanWright <tristan.wright@kitware.com>
2016-07-01 11:30:18 -0600

    initial commit

---
+++ src/convert.js
@@ -0,0 +1,65 @@
+/* eslint-disable */
+var template = require('./templates/output.hbs');
+
+module.exports = function (model) {
+    var templateData = { data: {}, valid: true , errors: []},
+        viewInstance = null,
+        count = 0,
+        list = null;
+
+    console.log('model: ', model);
+
+    function get(obj, prop) {
+        return obj[prop].value[0];
+    }
+
+    var calcSettings = model.data.settings[0].calculationSettings;
+    templateData.data.title = get(calcSettings, 'settings.title');
+    templateData.data.charge = get(calcSettings, 'settings.charge');
+
+    templateData.data.basis = '';
+    var theoryLib = get(calcSettings, 'settings.theory_basis');
+    templateData.data.theory_basis = theoryLib;
+    if (['cc-pVDZ', 'cc-pVTZ'].indexOf(theoryLib) !== -1) {
+        templateData.data.basis = 'spherical';
+    }
+
+    var theory = get(calcSettings, 'settings.theory_base');
+    templateData.data.theory_type = theory;
+    switch (theory) {
+        case 'scf':
+            // empty
+            break;
+        case 'dft':
+            var mult = get(calcSettings, 'settings.multiplicity');
+            templateData.data.theory = ['dft',
+                '  xc b3lyp',
+                '  mult ' + mult].join('\n');
+            break;
+        case 'mp2':
+            templateData.data.theory = ['mp2',
+                '  # Exclude core electrons from MP2 treatment',
+                '  freeze atomic'].join('\n');
+            break;
+        case 'ccsd':
+            templateData.data.theory = ['ccsd',
+                '  # Exclude core electrons from MP2 treatment',
+                '  freeze atomic'].join('\n');
+            break;
+        default:
+            break;
+    }
+
+    templateData.data.calculation_type = get(calcSettings, 'settings.type');
+
+    console.log('template:', templateData);
+    var filename = get(calcSettings, 'settings.filename_base');
+    var ret = {
+        errors: templateData.errors,
+        model: model,
+        results: {}
+    };
+    ret.results[filename + '.nw'] =  template(templateData.data);
+
+    return ret;
+}
