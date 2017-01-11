81d087aedd81c822795766bdaede1ba41ad138a8
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-01-11 13:20:30 -0700

    Allow graceful failure when data model is empty

--- src/convert.js
+++ src/convert.js
@@ -4,8 +4,12 @@ module.exports = function convert(dataModel) {
   const results = {};
   const error = null;
 
-  const simulationPath = dataModel.data.ExampleSelection[0].example.path.value[0];
-  results['DockerOpenFOAM_run.sh'] = template({ simulationPath });
+  try {
+    const simulationPath = dataModel.data.ExampleSelection[0].example.path.value[0];
+    results['DockerOpenFOAM_run.sh'] = template({ simulationPath });
+  } catch (e) {
+    console.log('Error trying to convert model for generating DockerOpenFOAM_run.sh', e);
+  }
 
   return { results, error };
 };
