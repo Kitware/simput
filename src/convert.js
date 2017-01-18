b2fbdd32b56abe3cb8c36051d6794944fa02d6e8
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2017-01-18 16:39:04 -0700

    Better support default values

--- src/convert.js
+++ src/convert.js
@@ -7,38 +7,43 @@ module.exports = function (model) {
         count = 0,
         list = null;
 
-    console.log('model: ', model);
-
-    function get(obj, prop) {
+    function get(obj, prop, defaultValue) {
       try {
         var ret = obj[prop].value[0];
         return ret;
       } catch (e) {
-        return null;
+        return defaultValue;
       }
     }
 
-    var dft = model.data.dft[0].dft,
-        basis = model.data.basis[0].basis,
-        neb = model.data.neb[0].neb;
+    var dft = (model.data && model.data.dft && model.data.dft[0]) ? model.data.dft[0].dft : null,
+        basis = (model.data && model.data.basis && model.data.basis[0]) ? model.data.basis[0].basis : null,
+        neb = (model.neb && model.data.neb && model.data.neb[0]) ? model.data.neb[0].neb : null;
 
-    templateData.data.dft_maxiter = get(dft, 'dft.maxiter');
+    templateData.data.dft_maxiter = get(dft, 'dft.maxiter', 5000);
 
-    var theoryLib = get(basis, 'basis.theory_basis');
+    var theoryLib = get(basis, 'basis.theory_basis', '6-31G*');
     templateData.data.theory_basis = theoryLib;
     if (['cc-pVDZ', 'cc-pVTZ'].indexOf(theoryLib) !== -1) {
         templateData.data.basis = 'spherical';
     }
 
-    templateData.data.nbeads = get(neb, 'neb.nbeads');
-    templateData.data.kbeads = get(neb, 'neb.kbeads');
-    templateData.data.maxiter = get(neb, 'neb.maxiter');
-    templateData.data.stepsize = get(neb, 'neb.stepsize');
+    templateData.data.nbeads = get(neb, 'neb.nbeads', 10);
+    templateData.data.kbeads = get(neb, 'neb.kbeads', 0.1);
+    templateData.data.maxiter = get(neb, 'neb.maxiter', 20);
+    templateData.data.stepsize = get(neb, 'neb.stepsize', 1.0);
 
-    templateData.data.start_geometry = get(model.external, 'startGeometry');
-    templateData.data.end_geometry = get(model.external, 'endGeometry');
+    templateData.data.start_geometry = model.external.startGeometry || '';
+    templateData.data.end_geometry = model.external.endGeometry || '';
 
-    console.log('template:', templateData);
+    if (!templateData.data.start_geometry || !templateData.data.start_geometry.length) {
+      console.log('input model', model);
+      templateData.errors.push('The start geometry is empty');
+    }
+    if (!templateData.data.end_geometry || !templateData.data.end_geometry.length) {
+      templateData.errors.push('The end geometry is empty');
+    }
+
     var ret = {
         errors: templateData.errors,
         model: model,
