e2606cba08d1083ec049b8311b6020bba41eaff7
Forrest Li <forrest.li@kitware.com>
2018-08-10 16:12:53 -0400

    feat(CellProperty): Support multiple layout types and cell sizes

--- src/components/properties/CellProperty/script.js
+++ src/components/properties/CellProperty/script.js
@@ -13,6 +13,14 @@ export default {
   data() {
     return { help: false };
   },
+  computed: {
+    layout() {
+      return this.prop.ui.layout || '1';
+    },
+    size() {
+      return this.prop.ui.size || 1;
+    },
+  },
   methods: {
     isVisible() {
       return this.prop.show(this.viewData);
@@ -25,6 +33,33 @@ export default {
         newData.value[index] = value;
       }
       this.$emit('change', newData);
+    },
+    getCellLabel(idx) {
+      return this.prop.ui.component && this.prop.ui.component[idx];
+    },
+    getCellAttrs(cellIndex) {
+      // cell sizing
+      let size = 'xs1';
+      let offset = 'offset-xs0';
+      if (this.layout === '2x3') {
+        size = 'xs4';
+      } else if (this.layout === '3x2') {
+        size = 'xs6';
+      } else if (this.layout === 'm6') {
+        size = 'xs4';
+        if (cellIndex === 3) {
+          offset = 'offset-xs4';
+        } else if (cellIndex === 5) {
+          offset = 'offset-xs8';
+        }
+      } else if (this.size <= 12) {
+        size = `xs${Math.floor(12 / this.size)}`;
+      }
+
+      return {
+        [size]: true,
+        [offset]: true,
+      };
     },
   },
   beforeMount() {
