60da2d0d2ad2f0a0a583369d3ffcfddab86761b8
Forrest Li <forrest.li@kitware.com>
2018-08-10 16:30:03 -0400

    feat(CheckboxProperty): Add initial checkbox property

---
+++ src/components/properties/CheckboxProperty/index.vue
@@ -0,0 +1,3 @@
+<template src="simput/src/components/properties/CheckboxProperty/template.html" />
+<style module src="simput/src/components/properties/CheckboxProperty/style.css" />
+<script src="simput/src/components/properties/CheckboxProperty/script.js" />
--- src/components/properties/registerDefaults.js
+++ src/components/properties/registerDefaults.js
@@ -1,7 +1,9 @@
 import CellProperty from 'simput/src/components/properties/CellProperty';
 import EnumProperty from 'simput/src/components/properties/EnumProperty';
+import CheckboxProperty from 'simput/src/components/properties/CheckboxProperty';
 
 export default function registerDefaults(registerFn) {
   registerFn('cell', CellProperty);
   registerFn('enum', EnumProperty);
+  registerFn('checkbox', CheckboxProperty);
 }
