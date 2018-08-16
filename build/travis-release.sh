6b677d9607917e8f0470615a8cb873fc54086a9d
Travis CI <sebastien.jourdain@kitware.com>
2018-08-16 12:36:03 -0600

    ci(travis): Try to debug build issue

--- build/travis-release.sh
+++ build/travis-release.sh
@@ -5,12 +5,12 @@ set -ev
 NIGHTLY_VERSION=`npm info | grep latest | cut -d " " -f 2`
 echo "window.SIMPUT_VERSION = '$NIGHTLY_VERSION';" > dist/version.js
 
-npm run build:release
-npm run validate
-git config --global user.name "Travis CI"
-git config --global user.email "sebastien.jourdain@kitware.com"
-export GIT_PUBLISH_URL=https://${GH_TOKEN}@github.com/Kitware/simput.git
-npm run semantic-release
+# npm run build:release
+# npm run validate
+# git config --global user.name "Travis CI"
+# git config --global user.email "sebastien.jourdain@kitware.com"
+# export GIT_PUBLISH_URL=https://${GH_TOKEN}@github.com/Kitware/simput.git
+# npm run semantic-release
 
-# Generate website
-npm run doc:publish
+# # Generate website
+# npm run doc:publish
