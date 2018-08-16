af5fbcf5d18a0d7a7e84b9c9ae38a40696911c18
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-08-16 13:50:30 -0600

    ci(travis): Still trying to fix ci

--- build/travis-release.sh
+++ build/travis-release.sh
@@ -7,10 +7,13 @@ echo "window.SIMPUT_VERSION = '$NIGHTLY_VERSION';" > dist/version.js
 
 npm run build:release
 npm run validate
-git config --global user.name "Travis CI"
+git config --global user.name "Sebastien Jourdain"
 git config --global user.email "sebastien.jourdain@kitware.com"
 export GIT_PUBLISH_URL=https://${GH_TOKEN}@github.com/Kitware/simput.git
 npm run semantic-release
+
+# Needed if not a release for nightly bundle
+npm run bundle
 
 # Generate website
 npm run doc:publish
