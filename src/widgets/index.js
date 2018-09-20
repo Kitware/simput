e6c78c4eb965d41f7365ad42e6cb834a11fd8fb1
Forrest Li <forrest.li@kitware.com>
2018-09-20 13:52:55 -0400

    Remove TestProperty

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -1,4 +1,3 @@
-import TestProperty from './TestProperty';
 import CellEditor from './CellEditor';
 import RodEditor from './RodEditor';
 import AssemblyEditor from './AssemblyEditor';
@@ -8,7 +7,6 @@ import StateMapEditor from './StateMapEditor';
 
 function registerWidgets(Simput) {
   if (Simput.registerWidget) {
-    Simput.registerWidget('test', TestProperty);
     Simput.registerWidget('CellEditor', CellEditor);
     Simput.registerWidget('RodEditor', RodEditor);
     Simput.registerWidget('AssemblyEditor', AssemblyEditor);
